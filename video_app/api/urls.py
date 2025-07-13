3eef03f7156002c984ab30b71b65628cd30c314b
phischa <ph.git@mailbox.org>
2025-07-13 21:05:56 +0200

    dockercompose.yml changes

--- video_app/api/urls.py
+++ video_app/api/urls.py
@@ -1,2 +1,7 @@
 from django.urls import path, include
+from auth_app.api import urls as auth_app_urls
 from rest_framework.routers import DefaultRouter
+
+urlpatterns = [
+    path("", include(auth_app_urls)),
+]
