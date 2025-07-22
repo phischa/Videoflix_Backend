3a08d006e7e095d1acfb304a7b75cdb22281ae58
phischa <ph.git@mailbox.org>
2025-07-22 15:25:39 +0200

    Video Model1

--- video_app/models.py
+++ video_app/models.py
@@ -1,4 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+GENRE_CHOICES = [
+    ('action', 'Action'),
+    ('drama', 'Drama'),
+    ('comedy', 'Comedy'),
+    ('romance', 'Romance'),
+    ('thriller', 'Thriller'),
+    ('documentary', 'Documentary'),
+    ('animation', 'Animation'),
+]
 
 class Video(models.Model):
-    pass
+    title = models.CharField(max_length=100, db_index=True)
+    description = models.TextField(blank=True)
+    category = models.CharField(max_length=20, choices=GENRE_CHOICES, db_index=True)
