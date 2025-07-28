d1679eb5118ccf45030d0b9f8213195d7e87bfa7
phischa <ph.git@mailbox.org>
2025-07-28 16:04:51 +0200

    Video View for HLS

--- video_app/api/views.py
+++ video_app/api/views.py
@@ -1,4 +1,7 @@
+from django.http import Http404
 from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from auth_app.authentication import CookieJWTAuthentication 
 from video_app.models import Video
@@ -10,10 +13,18 @@ class VideoListView(generics.ListAPIView):
     serializer_class = VideoListSerializer
     authentication_classes = [CookieJWTAuthentication]  
     permission_classes = [IsAuthenticated]
-    #permission_classes = [AllowAny]
 
     def dispatch(self, request, *args, **kwargs):
         print(f"DEBUG: Request to VideoListView")
         print(f"DEBUG: Authentication classes: {self.authentication_classes}")
         print(f"DEBUG: Cookies in request: {list(request.COOKIES.keys())}")
         return super().dispatch(request, *args, **kwargs)
+    
+
+class HLSManifestView(APIView):
+    authentication_classes = [CookieJWTAuthentication]
+    permission_classes = [IsAuthenticated]
+    
+    def get(self, request, movie_id, resolution):
+        # TODO: Hier kommt die Logik
+        pass
