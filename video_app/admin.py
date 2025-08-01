be4e0917cc997c9755683c6861c86d8dcf122161
phischa <ph.git@mailbox.org>
2025-08-01 12:16:03 +0200

    admin video upload

--- video_app/admin.py
+++ video_app/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+
+@admin.register(Video)  
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ['title', 'category', 'processing_status', 'created_at']
+    list_filter = ['processing_status', 'category', 'created_at']
+    search_fields = ['title', 'description']
+    readonly_fields = ['processing_progress', 'processing_error', 'hls_directory', 'duration_seconds', 'file_size_mb']
