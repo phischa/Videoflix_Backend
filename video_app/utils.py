4914bd2f69a34ed6431157f3092ab4ea9692a0f5
phischa <ph.git@mailbox.org>
2025-08-08 10:23:06 +0200

    video_app utils.py documentation

--- video_app/utils.py
+++ video_app/utils.py
@@ -2,6 +2,14 @@ from django.conf import settings
 from django.core.exceptions import ValidationError
 
 def validate_file_size(value):
-    """Validates that uploaded file doesn't exceed MAX_FILE_SIZE"""
+    """
+    Validates that uploaded file doesn't exceed MAX_FILE_SIZE.
+    
+    Args:
+        value: File object with size attribute
+        
+    Raises:
+        ValidationError: If file size exceeds configured maximum
+    """
     if value.size > settings.MAX_FILE_SIZE:
         raise ValidationError("File size cannot exceed 10GB")
