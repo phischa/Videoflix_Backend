c244fbe6e40f5e6cf7b4f4cf957bea467b4b3016
phischa <ph.git@mailbox.org>
2025-08-08 10:06:50 +0200

    apps.py documentation

--- auth_app/apps.py
+++ auth_app/apps.py
@@ -2,5 +2,14 @@ from django.apps import AppConfig
 
 
 class AuthAppConfig(AppConfig):
+    """
+    Configuration class for the authentication app.
+    
+    Attributes:
+        default_auto_field: Default primary key field type
+        name: App name for Django registration
+    """
+    
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'auth_app'
+    verbose_name = 'Authentication and User Management'
